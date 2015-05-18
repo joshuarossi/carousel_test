23a038104bd8fdbe3c89ae8256f657ea9636bb77
joshuarossi <joshua.rossi@icloud.com>
2015-05-17 19:09:27 -0500

    added mouse over events

--- carousel_test.js
+++ carousel_test.js
@@ -1,9 +1,25 @@
 if (Meteor.isClient) {
-  Template.carousel.rendered = function() {
-    $('#carousel').slick({
-      dots: false,
-      arrows: true,
-      draggable: true
+    Session.setDefault({'button_hidden': true});
+    Template.carousel.rendered = function () {
+        $('#carousel').slick({
+            dots: false,
+            arrows: false,
+            draggable: true
+        });
+    };
+    Template.pageOne.helpers({
+        'buttonHidden': function () {
+            return Session.get('button_hidden')}
     });
-  }
+
+    Template.carousel.events({
+        'mouseenter #carousel': function () {
+            Session.set({'button_hidden': false});
+            console.log(Session.get('button_hidden'))
+        },
+        'mouseleave #carousel': function () {
+            Session.set({'button_hidden': true});
+            console.log(Session.get('button_hidden'))
+        }
+    })
 }
